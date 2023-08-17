d9ece91c6ad2c35240c6a3f3630658b9abd74d06
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-08-17 11:36:34 +0700

    Read data from Configuration File

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rust_email_newsletter::startup::run;
+use rust_email_newsletter::{startup::run, configuration::get_configuration};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8080")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
