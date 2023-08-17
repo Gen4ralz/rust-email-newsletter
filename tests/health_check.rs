0832843b31ede984f0c335eb48a5bd2cc92c5a6a
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-08-17 14:54:20 +0700

    Test isolation by spin new random db for each test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
 use std::{net::TcpListener, vec};
-use rust_email_newsletter::{startup::run, configuration::get_configuration};
-use sqlx::PgPool;
+use rust_email_newsletter::{startup::run, configuration::{get_configuration, DatabaseSettings}};
+use sqlx::{PgPool, PgConnection, Connection, Executor};
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -14,12 +15,10 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
-        &configuration.database.connection_string()
-    )
-    .await
-    .expect("Failed to connect to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
 
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
 
@@ -29,6 +28,31 @@ async fn spawn_app() -> TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+
+    let mut connection = PgConnection::connect(
+        &config.connection_string_without_db()
+    )
+    .await
+    .expect("Failed to connect to Postgres");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
 
 #[tokio::test]
