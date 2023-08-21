35b887bb0dc9cc306d663d848058041aa5a0df0e
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-08-21 18:54:50 +0700

    Refactoring code into telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
-use rust_email_newsletter::{configuration::get_configuration, startup::run};
+use rust_email_newsletter::configuration::get_configuration;
+use rust_email_newsletter::startup::run;
+use rust_email_newsletter:: telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
 
-    LogTracer::init().expect("Failed to set logger");
-
-    // Printing all spans at info-level or above if RUST_LOG env not been set
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new(
-        "rust_email_newsletter".into(), 
-        std::io::stdout
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("rust_email_newsletter".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,16 @@
 use rust_email_newsletter::{
     configuration::{get_configuration, DatabaseSettings},
-    startup::run,
+    startup::run, telemetry::{get_subscriber, init_subscriber},
 };
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::{net::TcpListener, vec};
 use uuid::Uuid;
+use once_cell::sync::Lazy;
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let subscriber = get_subscriber("test".into(), "debug".into());
+    init_subscriber(subscriber);
+});
 
 pub struct TestApp {
     pub address: String,
@@ -12,6 +18,9 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     let port = listener.local_addr().unwrap().port();
